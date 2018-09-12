d10593303a010080af1b5cd9fe399e72f8845ebe
yfang1644 <yfang1644@gmail.com>
2018-09-12 11:36:23 +0800

    yuv2bmp use 8bit grayscale

--- yuv2bmp.c
+++ yuv2bmp.c
@@ -22,50 +22,70 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#pragma    pack(2)
+struct FileHeader {
+    char bfType[2];
+    int bfSize;
+    short bfReserved1;
+    short bfReserved2;
+    int bfOffBits;
+};
+
+struct ImageHeader {
+    int biSize;
+    int biWidth;
+    int biHeight;
+    short biPlanes;
+    short biBitCount;
+    int biCompression;
+    int biSizeImage;
+    int biXPelsPerMeter;
+    int biYPelsPerMeter;
+    int biClrUsed;
+    int biClrImportant;
+};
+
 void bmphead(int width, int height, int pixelbits, int fd)
 {
     unsigned char *p;
-#pragma    pack(2)
+    unsigned int i, pallete[256];
+
     struct header {
-        char bmp[2];
-        int fsize;
-        short reserved1;
-        short reserved2;
-        int offset_data;
-        int headsize;
-        int pwidth;
-        int pheight;
-        short planes;
-        short bpp;
-        int compression;
-        int bmpsize;
-        int hres;
-        int vres;
-        int ncolors;
-        int importantcolor;
-    };
+        struct FileHeader fh;
+        struct ImageHeader ih;
+    } mybmp;
 
-    struct header mybmp;
-    
-    mybmp.bmp[0] = 'B'; mybmp.bmp[1] = 'M';
-    mybmp.reserved1 = 0;
-    mybmp.reserved2 = 0;
-    mybmp.offset_data = sizeof(mybmp);
-    mybmp.headsize = 40;
-    mybmp.pwidth = width;
-    mybmp.pheight = height;
-    mybmp.planes = 1;
-    mybmp.bpp = pixelbits;
-    mybmp.compression = 0;
-    mybmp.bmpsize = width * height * pixelbits/8;
-    mybmp.hres = 2834;
-    mybmp.vres = 2834;
-    mybmp.ncolors = 0;
-    mybmp.importantcolor = 0;
+    mybmp.fh.bfType[0] = 'B';
+    mybmp.fh.bfType[1] = 'M';
+    mybmp.fh.bfSize = 0;
+    mybmp.fh.bfReserved1 = 0;
+    mybmp.fh.bfReserved2 = 0;
+    mybmp.fh.bfOffBits = sizeof(struct ImageHeader)
+                       + sizeof(struct FileHeader)
+                       + sizeof(pallete);
 
-    mybmp.fsize = mybmp.offset_data+ mybmp.bmpsize;
+    mybmp.ih.biSize = sizeof(struct ImageHeader);
+    mybmp.ih.biWidth = width;
+    mybmp.ih.biHeight = height;
+    mybmp.ih.biPlanes = 1;
+    mybmp.ih.biBitCount = 8;
+    mybmp.ih.biCompression = 0;
+    mybmp.ih.biSizeImage = width * height * pixelbits/8;
+    mybmp.ih.biXPelsPerMeter = 2835;
+    mybmp.ih.biYPelsPerMeter = 2835;
+    mybmp.ih.biClrUsed = 0;
+    mybmp.ih.biClrImportant = 0;
+
+    mybmp.fh.bfSize = mybmp.fh.bfOffBits + mybmp.ih.biSizeImage;
+
+    for (i = 0; i < 256; i++) {
+        pallete[i] = (i << 16) | (i << 8) | i;
+    }
+
+    /* TODO:  needs pading for structure alignment  */
 
     write(fd, &mybmp, sizeof(mybmp));
+    write(fd, pallete, sizeof(pallete));
 }
 
 static inline int clip(int value, int min, int max) {
@@ -82,7 +102,7 @@ void yuv2rgb(int rfile, int wfile)
     unsigned int x,y;
     int y0,u,y1,v;
     unsigned int r,g,b;
-    unsigned int color[720*2];
+    unsigned char color[720*2];
     unsigned char p[720*4];
 
     for(y = 0; y < height; y++) {
@@ -94,15 +114,16 @@ void yuv2rgb(int rfile, int wfile)
             y1 = *in++;
             v  = (*in++) - 128;
 
+            /* for grayscale, r=g=b */
             r = y0 + ((351 * v) >> 8);
             r = clip(r, 0, 255);
-            color[x] = (r << 16) | (r << 8) | r;
+            color[x] = r;
 
             r = y1 + ((351 * v) >> 8);
             r = clip(r, 0, 255);
-            color[x+1] = (r << 16) | (r << 8) | r;
+            color[x+1] = r;
         }
-        write(wfile, color, 4*width);
+        write(wfile, color, width);
     }
 }
 
@@ -124,7 +145,7 @@ int main (int argc, char *argv[])
     fd1 = open(argv[1], O_RDONLY);
     fd2 = open(argv[2], O_RDWR|O_CREAT, 0644);
 
-    bmphead(720, 480, 32, fd2);
+    bmphead(720, 480, 8, fd2);
     lseek(fd1, 128, SEEK_SET);   /* skip file head */
 
     yuv2rgb(fd1, fd2);
